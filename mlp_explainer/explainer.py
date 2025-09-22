c7da9d47834c26af9719e99024403c13ad89e1ea
TumeloMkwambe <mkwambetumelo@gmail.com>
2025-09-22 19:59:17 +0200

    Checkpoint iii.

--- mlp_explainer/explainer.py
+++ mlp_explainer/explainer.py
@@ -3,98 +3,66 @@ import pandas as pd
 from pgmpy.estimators import PC, HillClimbSearch, GES
 
 class MLPExplainer:
-    def __init__(self, mlp, target_node):
-        self.__mlp = mlp
-        self.target_node = target_node
+    def __init__(self, mlp):
 
         self.bn = None
+        self.__mlp = mlp
         self.__X = None
         self.__Y = None
 
-    def dg_c(self, X, explain_X, n_samples = 1000, replacement_prob = 0.5):
-    
-        '''
-        emp_cov = np.cov(X, rowvar = False)
-    
-        explain_X = np.atleast_2d(explain_X)
-        n_features = explain_X.shape[1]
-    
-        diff_X_all = []
-        diff_Y_all = []
-    
-        for explain_x in explain_X:
-    
-            samples = np.random.multivariate_normal(mean = explain_x, cov = emp_cov, size = n_samples)
-    
-            mask = np.random.rand(n_samples, n_features) < replacement_prob
-            samples = np.where(mask, explain_x, samples)
-    
-            proc_explain_x = preprocessor(explain_x.reshape(1, -1))
-            explain_y = self.__mlp.predict(proc_explain_x)
-    
-            proc_samples = preprocessor(samples)
-            samples_y = self.__mlp.predict(proc_samples)
-    
-            diff_X = explain_x - samples
-            diff_Y = explain_y - samples_y.reshape(-1, 1)
-    
-            diff_X_all.append(diff_X)
-            diff_Y_all.append(diff_Y)
-    
-        diff_X_all = np.vstack(diff_X_all)
-        diff_Y_all = np.vstack(diff_Y_all)
-    
-        self.__X = diff_X_all
-        self.__Y = diff_Y_all
-        '''
-
-    def dg_d(self, X, explain_x, N = 10, replacement_prob = 0.5):
+    def __dg_d(self, x, X_train, preprocessor, n = 10, prob = 0.5):
 
         samples_X = []
         samples_Y = []
 
-        column_values = [np.unique(X[:,i]) for i in range(len(X[0]))]
+        processed_x = preprocessor(x_prime.reshape(1, -1))
+        y = self.__mlp.predict(processed_x)
 
-        explain_y = self.__mlp.predict(explain_x.reshape(1, -1))
-
-        for i in range(N):
+        for i in range(n):
             
-            sample = np.array([np.random.choice(column) for column in column_values])
+            # sample a random datapoint x_prime from the training set
+            idx = np.random.randint(len(X_train))
+            x_prime = X_train[idx]
 
-            mask = np.random.rand(len(sample)) < replacement_prob
-            sample = np.where(mask, explain_x, sample)
+            # replace random values in the sample with values from the original datapoint x
+            mask = np.random.rand(len(x_prime)) < prob
+            x_prime = np.where(mask, x, x_prime)
 
-            pred = self.__mlp.predict(sample.reshape(1, -1))
+            # preprocess x_prime the same way as the training set and get a prediction
+            processed_x_prime = preprocessor(x_prime.reshape(1, -1))
+            y_prime = self.__mlp.predict(processed_x_prime)
 
-            target_mask = pred.argmax() == explain_y.argmax()
-            sample = sample == explain_x
+            # model returns probabilities, so we have to find the class with the maximum
+            # if prediction changed mask target value as 1 and 0 otherwise
+            # mask values in x_prime that are different from x's as 1 and 0 otherwise
+            y_prime = y_prime.argmax() == y.argmax()
+            x_prime = x_prime == x
 
-            samples_X.append(sample.astype(int))
-            samples_Y.append(target_mask.astype(int))
+            # add masked sample and prediction to respective collections
+            samples_X.append(x_prime.astype(int))
+            samples_Y.append(y_prime.astype(int))
 
         self.__X = samples_X
         self.__Y = samples_Y
 
-    def variable_selection(self):
+    def __vs(self):
         '''
         Variable selection.
         '''
 
-    def structure_learning(self, x_columns):
+    def __sl(self, x_cols, y_col) -> None:
 
-        dataframe = pd.DataFrame(self.__X, columns = x_columns)
-        dataframe[self.target_node] = self.__Y
+        dataframe = pd.DataFrame(self.__X, columns = x_cols)
+        dataframe[y_col] = self.__Y
 
         est = HillClimbSearch(data = dataframe)
         self.bn = est.estimate(scoring_method = "bic-g", max_indegree = 5, max_iter = int(1e4))
-    
-    def run(self, X, explain_x, x_columns, n_samples, replacement_prob):
-        
-        if type(explain_x[0]) == float:
-            self.dg_c(X, explain_x, n_samples, replacement_prob)
-        if type(explain_x[0]) == int:
-            self.dg_d(X, explain_x, n_samples, replacement_prob)
 
-        # self.variable_selection()
+    def explain(self, x, X_train, preprocessor, x_cols, y_col, n, prob) -> None:
+
+        if type(x[0]) == int:
+            self.__dg_d(x, X_train, preprocessor, n, prob)
+
+        # self.__vs()
         
-        self.structure_learning(x_columns)
+        self.__sl(x_cols, y_col)
