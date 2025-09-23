10040727a395f23e0691251dada521ab77113d6c
TumeloMkwambe <mkwambetumelo@gmail.com>
2025-09-23 14:47:50 +0200

    Checkpoint iv.

--- mlp_explainer/explainer.py
+++ mlp_explainer/explainer.py
@@ -1,25 +1,26 @@
 import numpy as np
 import pandas as pd
-from pgmpy.estimators import PC, HillClimbSearch, GES
+from itertools import combinations
+from pgmpy.estimators import HillClimbSearch, BIC, CITests
 
-class MLPExplainer:
-    def __init__(self, mlp):
+class Explainer:
+    def __init__(self, model):
 
         self.bn = None
-        self.__mlp = mlp
-        self.__X = None
-        self.__Y = None
+        self.model = model
+        self.data = None
 
-    def __dg_d(self, x, X_train, preprocessor, n = 10, prob = 0.5):
+    def __data_generation(self, x, X_train, preprocessor, x_cols, y_col, n = 10, prob = 0.5):
 
-        samples_X = []
-        samples_Y = []
+        X = []
+        Y = []
 
-        processed_x = preprocessor(x_prime.reshape(1, -1))
-        y = self.__mlp.predict(processed_x)
+        processed_x = preprocessor(x.reshape(1, -1))
+        y = self.model.predict(processed_x, verbose = 0)
+        y = y.reshape(len(y[0]),)
 
         for i in range(n):
-            
+
             # sample a random datapoint x_prime from the training set
             idx = np.random.randint(len(X_train))
             x_prime = X_train[idx]
@@ -30,39 +31,55 @@ class MLPExplainer:
 
             # preprocess x_prime the same way as the training set and get a prediction
             processed_x_prime = preprocessor(x_prime.reshape(1, -1))
-            y_prime = self.__mlp.predict(processed_x_prime)
+            y_prime = self.model.predict(processed_x_prime, verbose = 0)
+            y_prime = y_prime.reshape(len(y_prime[0]),)
 
             # model returns probabilities, so we have to find the class with the maximum
-            # if prediction changed mask target value as 1 and 0 otherwise
-            # mask values in x_prime that are different from x's as 1 and 0 otherwise
+            # if prediction changed mask target value as 0 and 1 otherwise
+            # mask values in x_prime that are different from x's as 0 and 1 otherwise
             y_prime = y_prime.argmax() == y.argmax()
             x_prime = x_prime == x
 
             # add masked sample and prediction to respective collections
-            samples_X.append(x_prime.astype(int))
-            samples_Y.append(y_prime.astype(int))
+            X.append(x_prime.astype(int))
+            Y.append(y_prime.astype(int))
 
-        self.__X = samples_X
-        self.__Y = samples_Y
+        X = [X[i].astype(str) for i in range(len(X))]
+        Y = [Y[i].astype(str) for i in range(len(Y))]
 
-    def __vs(self):
-        '''
-        Variable selection.
-        '''
+        self.data = pd.DataFrame(self.__X, columns = x_cols)
+        self.data[y_col] = self.__Y
 
-    def __sl(self, x_cols, y_col) -> None:
+    def __variable_selection(self, target: str):
 
-        dataframe = pd.DataFrame(self.__X, columns = x_cols)
-        dataframe[y_col] = self.__Y
+        variables = list(self.data.columns)
+        S = {v: set() for v in variables}
 
-        est = HillClimbSearch(data = dataframe)
-        self.bn = est.estimate(scoring_method = "bic-g", max_indegree = 5, max_iter = int(1e4))
+        for var1, var2 in combinations(variables, 2):
+            test = CITests.chi_square(X = var1, Y = var2, Z = [], data = self.data, boolean = True, significance_level = 0.05)
+
+            if not test:
+                S[var1].add(var2)
+                S[var2].add(var1)
+        
+        U = {target}
+        
+        for node in S[target]:
+            U |= S[node]
+        
+        self.data = self.data[U]
+
+    def __structure_learning(self) -> None:
+
+        score = BIC(self.data)
+        est = HillClimbSearch(data = self.data)
+        
+        return est.estimate(scoring_method = score, max_indegree = 5, max_iter = int(1e3))
 
     def explain(self, x, X_train, preprocessor, x_cols, y_col, n, prob) -> None:
 
-        if type(x[0]) == int:
-            self.__dg_d(x, X_train, preprocessor, n, prob)
+        x = np.asarray(x)
 
-        # self.__vs()
-        
-        self.__sl(x_cols, y_col)
+        self.__data_generation(x, X_train, preprocessor, x_cols, y_col, n, prob)
+        self.__variable_selection(y_col)
+        self.__structure_learning()
--- mlp_explainer/metrics.py
+++ mlp_explainer/metrics.py
@@ -1,9 +1,7 @@
 import copy
 import numpy as np
 import networkx as nx
-from pgmpy.metrics import SHD
 import matplotlib.pyplot as plt
-from sklearn.metrics import f1_score
 from networkx.drawing.nx_agraph import graphviz_layout
 
 def draw_network(model) -> None:
@@ -42,19 +40,6 @@ def markov_blanket(model, target):
 
     return model_
 
-def get_f1_score(estimated_model, true_model):
-    
-    nodes = estimated_model.nodes()
-    
-    est_adj = nx.to_numpy_array(
-        estimated_model.to_undirected(), nodelist = nodes, weight = None
-    )
-    true_adj = nx.to_numpy_array(
-        true_model.to_undirected(), nodelist = nodes, weight = None
-    )
-
-    f1 = f1_score(np.ravel(true_adj), np.ravel(est_adj))
-    print("F1-score for the model skeleton: ", f1)
 
 def metrics(ground_bn, explainer_bn, target_node):
 
