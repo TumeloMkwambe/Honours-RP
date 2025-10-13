443c0ba18542937bbb9a55ca448fa5fbd9f36aaf
TumeloMkwambe <mkwambetumelo@gmail.com>
2025-10-13 09:55:23 +0200

    Update explainer.py

--- mlp_explainer/explainer.py
+++ mlp_explainer/explainer.py
@@ -1,94 +1,119 @@
 import numpy as np
 import pandas as pd
-from itertools import combinations
-from pgmpy.estimators import HillClimbSearch, BIC, CITests
+from mlxtend.frequent_patterns import fpgrowth, association_rules
 
 class Explainer:
-    def __init__(self, model):
-
-        self.bn = None
-        self.model = model
-        self.data = None
-
-    def __data_generation(self, x: np.ndarray, X_train: np.ndarray, preproc, x_cols: list, y_col: str, n: int = 10, prob: float = 0.5):
+    def __init__(self, model, X, preprocessor, n_samples = 100, rep_prob = 0.5):
 
         '''
         Args:
-            x: a single local datapoint forming the base reference from which we sample perturbed instances.
-            X_train: an array containing data used to train the model prior to preprocessing.
-            preproc: the function used to preprocess input data prior to a forward pass to the model.
-            x_cols: list of names of features.
-            y_col: name of target.
-            n: number of samples to draw from base reference local datapoint using perturbation scheme.
-            prob: probability of replacing a randomly drawn datapoint from the training data with values from the base reference local datapoint.
+            model: model
+            X: dataframe of X data (training, testing or all)
+            preprocessor: function used to preprocess data prior to model forward pass
+            n_samples: number of samples to generate for a single prediction explanation
+            rep_prob: probability at which each feature value of x datapoint should be replaced with a value from a sample
         '''
 
-        X = []
-        Y = []
-
-        proc_x = preproc.transform(x.reshape(1, -1))
-        y = self.model.predict(proc_x, verbose = 0)
-        y = y.reshape(len(y[0]),)
-
-        for i in range(n):
-
-            # sample a random datapoint x_prime from the training set
-            idx = np.random.randint(len(X_train))
-            x_prime = X_train[idx]
-
-            # replace random values in the sample with values from the original datapoint x
-            mask = np.random.rand(len(x_prime)) < prob
-            x_prime = np.where(mask, x, x_prime)
-
-            # preprocess x_prime the same way as the training set and get a prediction
-            proc_x_prime = preproc.transform(x_prime.reshape(1, -1))
-            y_prime = self.model.predict(proc_x_prime, verbose = 0)
-            y_prime = y_prime.reshape(len(y_prime[0]),)
-
-            # model returns probabilities, so we have to find the class with the maximum
-            y_prime = y_prime.argmax() == y.argmax() # 0 if target changed, else 1
-            x_prime = x_prime == x # 0 if feature value changed, else 1
-
-            # add masked sample and prediction to respective collections
-            X.append(x_prime.astype(int))
-            Y.append(y_prime.astype(int))
-
-        X = [X[i].astype(str) for i in range(len(X))]
-        Y = [Y[i].astype(str) for i in range(len(Y))]
-
-        self.data = pd.DataFrame(X, columns = x_cols)
-        self.data[y_col] = Y
-
-    def __variable_selection(self, target: str):
-
-        variables = list(self.data.columns)
-        S = {v: set() for v in variables}
-
-        for var1, var2 in combinations(variables, 2):
-            test = CITests.chi_square(X = var1, Y = var2, Z = [], data = self.data, boolean = True, significance_level = 0.05)
-
-            if not test:
-                S[var1].add(var2)
-                S[var2].add(var1)
+        self.model = model
+        self.X = X.to_numpy()
+        self.preprocessor = preprocessor
+        self.x_cols = X.columns
+        self.y_col = 'target'
+        self.n_samples = n_samples
+        self.rep_prob = rep_prob
         
-        U = {target}
+        self.data = None
+        self.patterns = []
+        self.relevance_dict = {col: 0 for col in self.x_cols}
+
+    def __init_structures(self):
+        self.data = None
+        self.patterns = []
         
-        for node in S[target]:
-            U |= S[node]
+    def __data_generation(self, x: np.ndarray):
+
+        y = self.model.predict(self.preprocessor(x.reshape(1, -1)), verbose = 0).squeeze(0)
+
+        samples_X = []
+        samples_Y = []
         
-        self.data = self.data[list(U)]
+        for i in range(self.n_samples):
+            
+            sample_x = self.X[np.random.randint(len(self.X))]
+            sample_x = np.where(np.random.rand(len(sample_x)) < self.rep_prob, x, sample_x)
 
-    def __structure_learning(self) -> None:
+            sample_y = self.model.predict(self.preprocessor(sample_x.reshape(1, -1)), verbose = 0).squeeze(0)
+            
+            sample_y = sample_y.argmax() != y.argmax()
+            sample_x = sample_x != x
+            
+            samples_X.append(sample_x.astype(int))
+            samples_Y.append(sample_y.astype(int))
+            
+        self.data = pd.DataFrame(samples_X, columns = self.x_cols)
+        self.data[self.y_col] = samples_Y
 
-        score = BIC(self.data)
-        est = HillClimbSearch(data = self.data)
+    def fp_growth(self, data, class_):
+
+        if class_ == 0:
+            data = 1 - data
+            
+        data = data.astype(bool)
+
+        class_patterns = fpgrowth(data, min_support = 0.3, use_colnames = True)
         
-        self.bn = est.estimate(scoring_method = score, max_indegree = 5, max_iter = int(1e3))
+        self.patterns.append(class_patterns)
 
-    def explain(self, x, X_train, preproc, x_cols, y_col, n, prob) -> None:
+    def __harmonic_merge(self):
 
-        x = np.asarray(x)
+        self.patterns[0] = self.patterns[0].rename(columns = {'support': 'support_stable'})
+        self.patterns[1] = self.patterns[1].rename(columns = {'support': 'support_unstable'})
+        
+        patterns_merged = pd.merge(
+            self.patterns[0],
+            self.patterns[1],
+            on = 'itemsets',
+            how = 'outer'
+        )
+        
+        patterns_merged = patterns_merged.fillna(0)
+        
+        support_0 = patterns_merged['support_stable']
+        support_1 = patterns_merged['support_unstable']
 
-        self.__data_generation(x, X_train, preproc, x_cols, y_col, n, prob)
-        self.__variable_selection(y_col)
-        self.__structure_learning()
+        denominator = support_0 + support_1
+        patterns_merged['Harmonic Mean'] = (2 * support_0 * support_1 / denominator).mask(denominator == 0, 0)
+        
+        harmonic_rank = patterns_merged[['itemsets', 'support_stable', 'support_unstable', 'Harmonic Mean']].sort_values(
+            by = 'Harmonic Mean', 
+            ascending = False
+        )
+        
+        return harmonic_rank[harmonic_rank['Harmonic Mean'] > 0.0]
+
+    def __relevance_rank(self):
+
+        rank = self.__harmonic_merge()
+
+        for row, idx in rank.iterrows():
+    
+            features = tuple(idx['itemsets'])
+            h_mean = idx['Harmonic Mean']
+            n_features = len(features)
+            
+            for feature in features:
+                self.relevance_dict[feature] += h_mean / n_features
+
+    def forward(self, x) -> None:
+
+        self.__init_structures()
+        
+        self.__data_generation(x)
+        
+        for class_ in [0, 1]:
+            
+            class_data = self.data.loc[self.data[self.y_col] == class_]
+            class_data = class_data.drop(self.y_col, axis = 1)
+            self.fp_growth(class_data, class_)
+        
+        self.__relevance_rank()
